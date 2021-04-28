880401878c6b782cf4d9c180246fb033749865cb
Dita Ibra <di44240@ubt-uni.net>
2021-04-28 17:19:37 +0200

    Create Log In & Register

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,16 +1,26 @@
 <template>
-<div class="container">
-    <div class="col-md-8">
-        <div class="card">Login</div>
-        <div v-if="error" class="alert aler-danger" >{{error}}</div>
-        <form action="#" @submit.prevent="submit">
-            
-            <input id="email" type="email" name="email"  required autofocus v-model="form.email"/><br>
-            <input id="password" type="text" name="password"  required  v-model="form.password"/><br>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
-</div>
-</div>
+<div class="auth-inner">
+    <div class="auth-log">
+        LOG IN
+    </div>
+    
+    <form>
+        <div class="form-group">
+        Username
+        <input type="username" class="form-control" placeholder="Username"/>
+        </div>
+
+        <div class="form-group">
+        Password
+        <input type="password" class="form-control" placeholder="Password"/>
+        </div>
+        
+        <button class="btn-block">LOG IN</button>
+
+        <div class="forgot-password">
+            <a href="#" class="forgot-password">Forgot the password?</a>
+        </div>
+    </form>
 </template>
 
 <script>
@@ -43,3 +53,68 @@ import firebase from "firebase";
     };
 
 </script>
+
+
+<style>
+
+.form-control {
+    height: 100%;
+    width: 100%;
+    border-color: grey;
+    box-shadow: none;
+    border-radius: 50px;
+    height: 40px;
+}
+
+.auth-inner{
+    width: 410px;
+    margin: auto;
+    background: #ffffff;
+    border: 1px solid rgb(112, 112, 112);
+    padding: 50px 55px 45px 55px;
+    transition: all .3s;
+    padding-left: 60px;
+}
+
+.auth-log{
+  text-align:center;
+  font-size: 25px;
+  font-weight: 400;
+  font-family: Oswald;
+  padding-top: 15px;
+  margin-bottom: 30px;
+}
+
+.form-control{    
+    height: 40px;
+    width: 310px;
+    margin-bottom: 30px;
+}
+
+.form-group{    
+  text-align:left;
+  font-family: Oswald;
+  color: black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 500;
+}
+
+.btn-block{
+    background-color: black;
+    color: white;
+    font-family: Oswald;
+    height: 30px;
+    width: 310px;
+    margin-bottom: 10px;
+}
+
+.forgot-password{
+    color:red;
+    text-align: right;
+    font-size: 14px;
+    font-family: oswald;
+    font-weight: 500;
+    padding-top: 20px;
+    margin-bottom: 20px;
+}
+</style>
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,16 +1,42 @@
 <template>
-<div class="container">
-    <div class="col-md-8">
-        <div class="card">Register</div>
-        <div v-if="error" class="alert aler-danger" >{{error}}</div>
-        <form action="#" @submit.prevent="submit">
-            <input id="email" type="email" name="email"  required autofocus v-model="form.email"/><br>
-            <input id="password" type="text" name="password"  required  v-model="form.password"/><br>
-            <button type="submit" class="btn btn-primary">Register</button>
-        </form>
-</div>
-</div>
+ <div class="auth-inner">
+    <div class="auth-log">
+        LOG IN
+    </div>
+    
+    <form>
+         
+       <div class="form-group">
+        Full Name
+        <input type="name" class="form-control" v-model="full_name" placeholder="Name"/>
+        </div>
+
+        <div class="form-group">
+        <important>Email</important>
+        <input type="email" class="form-control" v-model="email" placeholder="Email"/>
+        </div>
+
+        <div class="form-group">
+        <important>Username</important>
+        <input type="username" class="form-control" v-model="username" placeholder="Username"/>
+        </div>
+
+        <div class="form-group">
+        <important>Password</important>
+        <input type="password" class="form-control" v-model="password" placeholder="Password"/>
+                
+            <div class="login">
+                <a href= "/Login" class="login">Already have an account?</a>
+            </div>
+        
+        </div>
+        <button class="btn-block">Register</button>
+
+</form>
+ </div>
 </template>
+
+
 
 <script>
 import firebase from "firebase";
@@ -45,3 +71,79 @@ import firebase from "firebase";
     };
 
 </script>
+
+
+
+<style>
+
+.auth-inner{
+    width: 410px;
+    margin: auto;
+    background: #ffffff;
+    border: 1px solid rgb(112, 112, 112);
+    padding: 50px 55px 45px 55px;
+    transition: all .3s;
+    padding-left: 60px;
+}
+
+
+.form-control {
+    height: 100%;
+    width: 100%;
+    border-color: grey;
+    box-shadow: none;
+    border-radius: 50px;
+    height: 40px;
+}
+
+.auth-text{
+    
+  text-align:center;
+  font-size: 40px;
+  font-weight: 400;
+  font-family: Oswald;
+    padding-top: -14px ;
+}
+
+.auth-log{
+  text-align:center;
+  font-size: 25px;
+  font-weight: 400;
+  font-family: Oswald;
+  padding-top: 20px;
+
+}
+
+.form-control{    
+    
+    height: 40px;
+    width: 310px;
+}
+
+.form-group{    
+  text-align:left;
+  font-family: Oswald;
+  color: black;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 500;
+  padding-top: 15px ;
+}
+
+.btn-block{
+    background-color: black;
+    color: white;
+    font-family: Oswald;
+    height: 30px;
+    width: 310px;
+}
+
+.login{
+    color:grey;
+    text-align: left;
+    font-size: 14px;
+    font-family: oswald;
+    font-weight: 500;
+    padding-top: 8px;
+    
+}
+</style>
