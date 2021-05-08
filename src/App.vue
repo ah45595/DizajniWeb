b429c7bb34b081392b50c3a1a64f2b398ce1c817
Dita Ibra <di44240@ubt-uni.net>
2021-05-08 15:43:41 +0200

    Update and Change App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="App" :class="mode">
+   <Header :mode="mode" @toggle="toggle" />
     <app-Header></app-Header>
     <div class="container">
       <router-view></router-view>
@@ -10,15 +11,42 @@
 
 <script>
 /* eslint-disable */
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
+import Header from "@/components/Header";
+import Home from "@/components/Home";
+import Footer from "@/components/Footer";
 import Slider from "./components/Home-Slider.vue";
 import Smartphones from "./components/Smartphones.vue";
+
 export default {
+  name: 'App',
   data() {
     return {
       text: "Hello World",
     };
+  },
+   data(){
+    return{
+      mode: 'light'
+    }
+  },
+  created (){
+    window.addEventListener('keyup', this.keyPress)
+  },
+
+  methods: {
+    keyPress (e){
+      if(e.key === 'd'){
+        this.toggle()
+      }
+    },
+
+    toggle () {
+      if(this.mode === "dark"){
+        this.mode = "light";
+      } else {
+        this.mode = "dark";
+      }
+    }
   },
   filters: {
     toUpperCase(value) {
@@ -26,10 +54,9 @@ export default {
     },
   },
   components: {
-    appHeader: Header,
-    appFooter: Footer,
-    Slider,
-    Smartphones
+    appFooter:Footer,
+    Header,
+    Slider
   },
   directives: {
     "local-hightlight": {
@@ -38,7 +65,7 @@ export default {
       },
     },
   },
-};
+}
 </script>
 
 <style>
