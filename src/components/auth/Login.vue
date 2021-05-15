e4d4db308406337f68adcbdff2b6b892a646a747
ViolaSalihi <74013861+violasalihi@users.noreply.github.com>
2021-05-15 12:39:30 +0200

    Changes

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -41,7 +41,7 @@
 
               <div class="form-group row mb-0">
                 <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Login</button>
+                  <button type="submit" class="btn btn-primary login">Login</button>
                 </div>
               </div>
             </form>
@@ -114,5 +114,8 @@ export default {
 }
 .card{
     width: 450px;
+}
+.login{
+  padding-bottom:30px;
 }
 </style>
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -58,7 +58,7 @@
 
               <div class="form-group row mb-0">
                 <div class="col-md-8 offset-md-4">
-                  <button type="submit" class="btn btn-primary">Register</button>
+                  <button type="submit" class="btn btn-primary register">Register</button>
                 </div>
               </div>
             </form>
@@ -136,6 +136,9 @@ export default {
 }
 .card{
     width: 450px;
+}
+.register{
+  padding-bottom:30px;
 }
 
 </style>
